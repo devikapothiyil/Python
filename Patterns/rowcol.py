5404e4b922b7f6b0b9f11bf463076f2286205d7b
Devika Rajeev <145443027+devikapothiyil@users.noreply.github.com>
2025-02-15 12:58:02 +0530

    Create rowcol.py

---
+++ Patterns/rowcol.py
@@ -0,0 +1,12 @@
+ r=int(input("Enter the no of rows:"))
+ c=int(input("Enter the no of columns:"))
+ for i in range(r):
+     for j in range(c):
+         print("*",end=" ")
+     print()
+
+ output
+ * * * 
+ * * * 
+ * * * 
+ * * * 
