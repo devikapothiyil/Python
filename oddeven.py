e6238fb33bd32e3b010177e06f257ef266f70b00
Devika Rajeev <145443027+devikapothiyil@users.noreply.github.com>
2025-07-09 06:23:51 +0530

    Update oddeven.py

--- oddeven.py
+++ oddeven.py
@@ -3,3 +3,8 @@ if(num%2==0):
     print(num," is even")
 else:
     print(num," is odd")
+
+# def check_even_odd(n):
+#     return "Even" if n % 2 == 0 else "Odd"
+
+# print(check_even_odd(7))  # Odd
