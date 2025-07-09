3b485ded852c9d1a7b0619f752097ca7f91f221a
Devika Rajeev <145443027+devikapothiyil@users.noreply.github.com>
2025-07-09 06:16:50 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,9 +1,16 @@
-listorg=["m","a","k","a","m"]
-print(listorg)
-listcp=listorg.copy()
-listcp.reverse()
-print(listcp)
-if(listorg==listcp):
-    print("Palindrome!!")
-else:
-    print("Not Palindrome!!")
+def is_palindrome(s):
+    return s == s[::-1]
+
+print(is_palindrome("madam"))  # True
+
+
+
+# listorg=["m","a","k","a","m"]
+# print(listorg)
+# listcp=listorg.copy()
+# listcp.reverse()
+# print(listcp)
+# if(listorg==listcp):
+#     print("Palindrome!!")
+# else:
+#     print("Not Palindrome!!")
